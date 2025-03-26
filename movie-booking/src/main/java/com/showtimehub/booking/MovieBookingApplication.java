bfbf7da982f3af6aec77ed6ca9487eae7a602bdb
LAKSHMIKANT D <fullstacktrainer4u@gmail.com>
2025-03-26 13:18:34 +0530

    initial code

---
+++ movie-booking/src/main/java/com/showtimehub/booking/MovieBookingApplication.java
@@ -0,0 +1,13 @@
+package com.showtimehub.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingApplication.class, args);
+	}
+
+}
---
+++ movie-booking/src/test/java/com/showtimehub/booking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showtimehub.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
